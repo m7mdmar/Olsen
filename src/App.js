309220a3999115f4f02619d8360933038c737aa0
Ahmed Essam <bloxed7@gmail.com>
2022-10-30 18:41:08 +0200

    added container class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Button from "./components/Button/Button";
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Button>TEXT</Button>
+        </div>
+    );
 }
 
 export default App;
